f2623a5f0d7050f3d4603a8ae9a40fd24a743789
yvesdj <yves.dejonghe@student.ap.be>
2020-06-01 19:38:15 +0200

    Enemy targets player when within range, needs Raycasting

--- Assets/Scripts/Enemy/EnemyGunHandler.cs
+++ Assets/Scripts/Enemy/EnemyGunHandler.cs
@@ -8,6 +8,12 @@ public class EnemyGunHandler : MonoBehaviour
 
     public ParticleSystem muzzleFlash;
 
+    public float maxEngageRange;
+    private bool isEngaging;
+
+    public float fireRate = 5f;
+    private float _nextTimeToFire = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +23,42 @@ public class EnemyGunHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.transform);
-        muzzleFlash.Play();
+        isEngaging = IsInRange();
+
+        if (isEngaging)
+        {
+            transform.LookAt(target.transform);
+            
+            Shoot();
+        }
+
+    }
+
+    private bool IsInRange()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < maxEngageRange)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time >= _nextTimeToFire)
+        {
+            _nextTimeToFire = Time.time + 1f / fireRate;
+            muzzleFlash.Play();
+        }
+        
+        //_audioSource.Play();
+
+        //_recoilHandler.SetRecoil();
+
+        //_animator.SetBool("isFiring", true);
+
+        //CheckHit();
     }
 }
