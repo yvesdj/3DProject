c58e27c4d84b254471c58df443ad69f1d7046e7f
yvesdj <yves.dejonghe@student.ap.be>
2020-06-01 17:50:52 +0200

    Enemy always targets player, even without line of sight

---
+++ Assets/Scripts/Enemy/EnemyGunHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGunHandler : MonoBehaviour
+{
+    public GameObject target;
+
+    public ParticleSystem muzzleFlash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = Enemy
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform);
+        muzzleFlash.Play();
+    }
+}
