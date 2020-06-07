cff2eeac8c676221560d12312c1d26fa9bb024fa
Arne Janssens <arne.janssens@student.ap.be>
2020-06-07 16:16:04 +0200

    Added working door triggers and end level triggers

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    bool isOpened = false;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            door.transform.position += new Vector3(0, -5, 0);
+            isOpened = true;
+        }
+    }
+}
