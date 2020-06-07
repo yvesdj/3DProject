c7d3677bb2e9522e7806619a95fa313e69020e33
yvesdj <yves.dejonghe@student.ap.be>
2020-06-07 12:59:03 +0200

    Added custom soundeffects, SoundEffectPicker will pick a random sound for some actions

---
+++ Assets/Scripts/Player/SoundEffectPicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectPicker : MonoBehaviour
+{
+    public AudioClip[] hitClips;
+    public AudioClip[] dieClips;
+
+    public AudioSource audioSource;
+    public AudioListener audioListener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponentInChildren<AudioSource>();
+        audioListener = GetComponentInChildren<AudioListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayRandomHit()
+    {
+        audioSource.clip = hitClips[Random.Range(0, hitClips.Length)];
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void PlayRandomDie()
+    {
+        audioSource.clip = dieClips[Random.Range(0, dieClips.Length)];
+        audioSource.Play();
+    }
+}
