054979a8622c0102bd1904ef67af0313f7c14e4a
yvesdj <yves.dejonghe@student.ap.be>
2020-06-08 14:07:17 +0200

    Added a spin to the pickups

---
+++ Assets/Scripts/Pickups/PickupSpinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpinner : MonoBehaviour
+{
+    public float degreesPerSecond;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * degreesPerSecond, 0);
+    }
+}
