b8bcdcd99d99ccb7ab1b912eee6a34b834a2dc6d
yvesdj <yves.dejonghe@student.ap.be>
2020-06-09 01:21:20 +0200

    Score is represented in UI, ScoreAdded needs to fade away

--- Assets/Scripts/Player/ScoreHandler.cs
+++ Assets/Scripts/Player/ScoreHandler.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreHandler : MonoBehaviour
 {
     public float score;
 
+    public TextMeshProUGUI scoreTotal;
+    public TextMeshProUGUI scoreAdded;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
     public void AddScore(float amount)
     {
         score += amount;
+        scoreAdded.text = "+" + amount.ToString();
+        UpdateTotalScore();
         Debug.Log("Score: " + score);
+    }
+
+    public void UpdateTotalScore()
+    {
+        scoreTotal.text = score.ToString();
     }
 
 
