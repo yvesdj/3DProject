ca58e9a0971512226dab2eb646489f279e4c468f
yvesdj <yves.dejonghe@student.ap.be>
2020-06-09 23:55:50 +0200

    EndScore gets displayed when boss is killed, not saved, Reduced mvmntSpeed

---
+++ Assets/Scripts/UI/BossLevelScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BossLevelScore : MonoBehaviour
+{
+    public TextMeshProUGUI scoreTotal;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTotal.text = player.GetComponent<ScoreHandler>().score.ToString();
+    }
+}
